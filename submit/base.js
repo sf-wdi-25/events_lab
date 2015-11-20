5538b3be75c7feb6ca3038601c87af591c351172
isangieri <isabelleangieri@gmail.com>
2015-11-19 22:20:21 -0800

    Much trouble with all of this. Some questions were very unclear to me. Others simply too difficult to answer.

--- submit/base.js
+++ submit/base.js
@@ -2,6 +2,8 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  $("#time").text( Date.now() );
+$('input').eq(1).remove();
 
-})
+});
+
+$('form').eq(1).append("action", ("<iframe width="640" height="360" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>"));
