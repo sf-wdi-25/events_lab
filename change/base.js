2182ae1f5bb137dd285a1449be76809982788ef7
Joe Roers <Joe@Joe-Roers-MacBook-Pro.local>
2015-11-20 00:08:08 -0800

    W1 D4 Homework

--- change/base.js
+++ change/base.js
@@ -3,5 +3,8 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
-
-})
+//I honestly can't figure this out for the life of me
+  $("input.add").change(function add(event) {
+  	$("#total") = ($("input#left").val()) + ($("input#right").val());
+  });
+});
--- keypress/base.js
+++ keypress/base.js
@@ -1,5 +1,26 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+//.delay(1000) for seconds
+//Math.floor(minutes) + ":" + Math.floor(seconds % 60)
+  var start;
+  var end;
+  $(document).ready(function() {
+    $(window).on("keypress", function doTheKeypressThing(event) {
+    	if (start) {
+    		end = Date.now();
+    		var minutes = ((end - start) / 1000) / 60;
+    		var seconds = (((end - start) / 1000) % 60)
+    		if (seconds < 10) {
+    			$("#total-time").text(Math.floor(minutes) + ":0" + Math.floor(seconds));
+    		} else {
+    			$("#total-time").text(Math.floor(minutes) + ":" + Math.floor(seconds));
+    		}
+    	} else {
+    		start = Date.now();
+    		console.log("starting to count");
+    	}
+    });
+  });
 
-})
+});
