a8e2bd88e29508d20f760c28282f8ef57be3e634
kehontas <kehontas@gmail.com>
2015-11-19 19:04:13 -0800

    attempt at lab

--- keypress/base.js
+++ keypress/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+%("span").text("onkeypress", (function handleClick(event){
+        $("span").text( Date.now());
+
 
 })
--- ready/base.js
+++ ready/base.js
@@ -1,12 +1,12 @@
 /*
-  What needs to change so that the page says "Go!"?
+You can edit the text and remove the second line
 */
 
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
+  console.log( "The page says:", $("h1").text("Go!") );
 });
 
-$("h1").text("Go!");
+//$("h1").text("Go!");
 
 if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
