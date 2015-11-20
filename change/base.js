24bdc2b28eeb9ab70c16cc91349dcd6d4244c97c
bw-giraffe <theletterrsquared@gmail.com>
2015-11-19 19:05:01 -0800

    fumbled on change, others not as bad. would love to know more

--- change/base.js
+++ change/base.js
@@ -5,3 +5,5 @@ $(document).ready(function(){
   $("#time").text( Date.now() );
 
 })
+// I don't know what I'm doing
+// $('input').change(function() { ".input#left" + ".input#right" = ".input#total" });
--- keypress/base.js
+++ keypress/base.js
@@ -2,4 +2,16 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+var count = 0;
+
+
+$('body').keyup(function (event) {
+	if(event.keyCode == 32) {
+		//create switch
+	var counterOn = true; 
+	} if(counterOn) {
+		$("span").text(count++);
+	} 
+});
+
 })
