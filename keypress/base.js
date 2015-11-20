ed3068b41d737649ec634bead52907f28c0a45be
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-19 20:20:37 -0800

    Unable to solve any of these; nor wrap-up nav bar

--- keypress/base.js
+++ keypress/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+ $(document).keypress(function startTimer(event) {
 
+ })
 })
+//As far as I got without any of the provided reference material (http://api.jquery.com/category/events/) showing timer functions
