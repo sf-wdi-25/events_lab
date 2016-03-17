4e0ee47e5a8df048f6d1438d3bb1b5bf5556691f
mradambeck <mr.adambeck@gmail.com>
2016-03-17 15:58:56 -0700

    Finished #4, click - though the timestamp is a little weird

--- click/base.js
+++ click/base.js
@@ -2,4 +2,15 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $("span").on("click", function handleClick(event){
+      console.log(this);
+      $('ul').append('<li>' + $(this).text() + ' - ' + event.timeStamp + '</li>');
+  });
+
 })
+
+
+
+// ###4. click
+// - **Challenge**: Can you create a list of all the phrases that you click?
+// - Stretch: In addition to listing the phrase I clicked, can you include a timestamp?
