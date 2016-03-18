8e19e1a71af20ebc7fdb7c0ad035384ca69f5d1b
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-17 17:37:57 -0700

    added the timeStamp to the click problem

--- click/base.js
+++ click/base.js
@@ -1,9 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+//makes a list of the buttons clicked
 
   $('span').on('click', function trackClicks(event) {
-    $('ul').append('<li> '+ $(this).text() +'</li>');
+    $('ul').append('<li> '+ $(this).text() + " at " + event.timeStamp +'</li>');
   });
-  
+
 })
