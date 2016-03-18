b1bf4e8170942b2366e50f5f8783f64f59a28ae7
cloverharvest <anna@schwabdev.com>
2016-03-17 18:03:42 -0700

    Submitting answer for click problem

--- click/base.js
+++ click/base.js
@@ -1,5 +1,13 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+// click
+// - **Challenge**: Can you create a list of all the phrases that you click?
+// - Stretch: In addition to listing the phrase I clicked, can you include a timestamp?
 
-})
+
+$(document).ready(function(){
+    $('span').on('click', function listAdder(event) {
+    var newPhrase = 'This is what you clicked: ' + $(this).text();
+    $('ul'). append('<li>' + newPhrase + '</li>');
+  });
+});
