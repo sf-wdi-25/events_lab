2778f733e83eb2b9f0f92c9fb14df2bb2de6df3d
morgansharif <morgan.sharif@gmail.com>
2016-03-17 18:46:09 -0700

    click and partial keypress

--- click/base.js
+++ click/base.js
@@ -1,5 +1,15 @@
+// 4. click
+//- **Challenge**: Can you create a list of all the phrases that you click?
+//- Stretch: In addition to listing the phrase I clicked, can you include a timestamp?
+
+
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+var clickedItems = [];
+  $("span").on("click", function listClicks(event){
+        console.log(this);
+        $("ul").append("<li>" + $(this).text() + "</li><li>@" + event.timeStamp + "</li>");
+});
 
-})
+});
--- keypress/base.js
+++ keypress/base.js
@@ -1,5 +1,34 @@
+//5. keypress
+// - **Challenge**: Stop watch. When the user hits spacebar, record their "start" time. When they  hit it again, record their "end" time. Then, calculate the total time, and put it on the page.
+
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+//vv MY CODE BELOW vv
 
-})
+var startTime = 1458260807525;
+console.log("startTime = ", startTime);
+
+$(document).on("keypress",
+  function(event){
+    if (startTime === 0){
+      startTime = Date.now();
+      console.log("startTime capture:", startTime);
+    } else {
+      endTime = Date.now();
+      timePassed = endTime - startTime;
+      console.log("endTime capture:", endTime);
+      console.log("timePassed:", timePassed);
+      milliseconds = timePassed % 1000;
+      seconds = (((timePassed - milliseconds)/1000) % 60);
+      minutes = (seconds - (seconds % 60));
+      console.log("Minutes: ", minutes);
+      console.log("Seconds: ", seconds);
+      console.log("Milliseconds: ", milliseconds);
+
+    }
+  }
+);
+
+//^^ MY CODE ABOVE ^^
+});
