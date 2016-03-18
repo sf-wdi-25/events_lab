b99f90d7f0f4731ec14c1ffa76a32d92cb98f792
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-03-18 01:41:23 -0700

    Solutions

--- keypress/base.js
+++ keypress/base.js
@@ -1,5 +1,17 @@
-console.log("Sanity Check: JS is working!");
+//could not get this one
 
-$(document).ready(function(){
+var startTime; // undefined at first
+var endTime; // undefined
+var total; // undefined
 
-})
+$(window).on("keypress", function handleKeypress(event) {
+
+  if (startTime) {
+    endTime = Date.now();
+    total = (endTime - startTime) / 1000;
+    $("#total-time").text(total + " seconds");
+  } else {
+    startTime = Date.now();
+  }
+
+});
