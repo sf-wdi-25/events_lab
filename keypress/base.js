59cc448180ee9ea45dbb1cd650b6a907d96a8328
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-17 19:23:47 -0700

    added keypress

--- keypress/base.js
+++ keypress/base.js
@@ -2,4 +2,50 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+//code below
+/*
+
+$(window).on("keypress", function (e) {
+  if (e.keyCode === 0) {
+    e.preventDefault()
+    console.log('Space pressed at' + event.timeStamp)
+  }
+});
+
+*/
+
+
+var startTime;
+var endTime;
+var counter = 0;
+
+ $(window).on("keypress", function recordEnd(event) {
+
+   //When keyPress is false, get a start time
+   console.log(event.keyCode);
+    if (event.keyCode === 32) {
+      counter++;
+        if (counter === 1) {
+          startTime = Date.now();
+        } else {
+          endTime = Date.now();
+          console.log(endTime);
+          var totalTime = endTime - startTime;
+          console.log(totalTime > 0, totalTime);
+          if (totalTime > 0) {
+            $('span').text(totalTime);
+            console.log("if works!");
+          }
+        }
+    }
+ });
+
+
+
+ /*pseudo code
+
+ write a code that will append the totalTime to
+
+ */
+
 })
