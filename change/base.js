4c3f6db37fcf8d21339721c06a455e7be68d7475
lamchristine <christine.lam.06@gmail.com>
2016-03-18 00:51:57 -0700

    events lab-change

--- change/base.js
+++ change/base.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
   $("#time").text( Date.now() );
 
 })
+
+//
+// 3. change
+//
+// Challenge: Some simple addition! Can you get the total to update whenever you update the numbers?
+// Bonus: Add a "reset" button that clears all the inputs.
+
+
+parseInt ( $("#left").val(0) );
+parseInt ( $("#right").val(0) );
+
+
+$(".add").on("keyup click", function something(event) {
+          parseInt( $(".add").val() );
+          $("#total").val( parseInt( $("#left").val() ) + parseInt( $("#right").val() ) );
+       });
