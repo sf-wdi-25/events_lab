b9fcef45514e38ab98ac19112040c807cf766951
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-17 22:38:35 -0700

    milliseconds to seconds

--- keypress/base.js
+++ keypress/base.js
@@ -2,35 +2,32 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+/*
+
+Fun fact: while debugging some issues in my code along with a T.A. named Justin, we learned that
+NaN is a number! typeof NaN === "number"
+
+Wuhhh???
+
+*/
 
 var startTime;
 var endTime;
 var counter = 0;
 
  $(window).on("keypress", function recordEnd(event) {
-
-   //When keyPress is false, get a start time
-   console.log(event.keyCode);
     if (event.keyCode === 32) {
       counter++;
-        if (counter === 1) {
-          startTime = Date.now();
-        } else {
+      if (counter === 1) {
+        startTime = Date.now();
+      } else {
           endTime = Date.now();
           var totalTime = endTime - startTime;
           if (totalTime > 0) {
-            $('span').text(totalTime + ' milliseconds');
-          }
+          $('span').text( (totalTime/1000) + ' seconds');
         }
+      }
     }
  });
-
-
-
- /*pseudo code
-
- write a code that will append the totalTime to
-
- */
 
 })
