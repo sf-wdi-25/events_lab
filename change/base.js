022fbde29d81e54ef4698ef3cf4b90a4dcfad4c1
mradambeck <mr.adambeck@gmail.com>
2016-03-17 22:04:53 -0700

    having issue with change - i believe jquery is reading the input values as strings and not integers. will try agin in the am.

--- change/base.js
+++ change/base.js
@@ -2,6 +2,26 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  $("#time").text( Date.now() );
+  $("#time").text( Date.now() + " yr mom" );
 
-})
+  // sets all values to 0
+  $("input").val(0);
+
+
+  $("input").on("click", function doMath(left, right){
+
+    // var $left = $('#left').val();
+    // var $right = $('#right').val();
+    // var $total = $left + $right;
+//    $('#total').val('#left' + '#right');
+    $('#total').val (
+      $('#left').val() + $('#right').val()
+    );
+  });
+
+  // $('article').text("Boo!")
+  // <input type="number" class="add" id="left"> +
+  // <input type="number" class="add" id="right"> =
+  // <input type="number" id="total"><br>
+});
+// set the total box to be the sum of left and right
