1aa09c385300cfd5129fef66b299cfa6bc161177
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-17 17:27:09 -0700

    solved Click problem

--- click/base.js
+++ click/base.js
@@ -2,4 +2,8 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $('span').on('click', function trackClicks(event) {
+    $('ul').append('<li> '+ $(this).text() +'</li>');
+  });
+  
 })
