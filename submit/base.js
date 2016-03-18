3deb455c772e4bde89e3fe59fa67753ef1328d37
Hila Vasiler <hila.marmel@gmail.com>
2016-03-18 09:13:25 -0700

    jQuery HW

--- submit/base.js
+++ submit/base.js
@@ -1,7 +1,17 @@
+/*
+Challenge: Can you stop the forms from submitting?
+Why does one button reload the same page and the other redirect to youtube?
+--> The first submit doesnt have an action so it doesnt do anything
+  and the second submit has an action that tells it to redirect
+  to youtube.
+*/
+
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
-
-})
+  
+  $("form input").on("click", function handleClick(event)
+    {event.preventDefault(); alert("No Submit for You!!");});
+});
